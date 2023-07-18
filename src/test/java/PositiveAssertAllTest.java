083b9125706ff5d8d3b528b6436f7f67372d76fa
Oleg_Barbashin <banksnab@gmail.com>
2023-07-19 01:06:07 +0700

    commit with value source parameters

--- src/test/java/PositiveAssertAllTest.java
+++ src/test/java/PositiveAssertAllTest.java
@@ -1,17 +1,26 @@
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import pojo.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import pojo.Category;
+import pojo.Detective;
+import pojo.DetectivesResponse;
+import pojo.Extra;
 
 import java.util.List;
 
-import static generator.Helper.*;
+import static generator.Helper.createTwoDetectiveResponse;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class PositiveAssertAllTest {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "\"Sherlock\", true",
+            "\"NotSherlok\", false"
+    })
     @DisplayName("Проверка всех условий валидности json в одном тесте")
-    public void testPositiveAssertAll() {
+    public void testPositiveAssertAll(String name, boolean isSherlock) {
         DetectivesResponse detectivesResponse = createTwoDetectiveResponse(
                 1,
                 2,
@@ -21,63 +30,64 @@ public class PositiveAssertAllTest {
                 2,
                 1,
                 2,
-                "Sherlock",
+                name,
                 "Tom",
                 "Holmes",
                 "Cat",
                 true,
                 true,
                 false,
-                true
+                isSherlock
         );
-        assertAll("Detectives",
-                () -> {
-                    List<Detective> detectives = detectivesResponse.getDetectives();
 
-                    // Массив detectives может иметь не менее одного и не более 3-х объектов
-                    assertTrue(detectives.size() >= 1 && detectives.size() <= 3,
-                            "Ошибка: Массив detectives должен содержать от 1 до 3 объектов");
+            assertAll("Detectives",
+                    () -> {
+                        List<Detective> detectives = detectivesResponse.getDetectives();
 
-                    for (Detective detective : detectives) {
-                        int mainId = detective.getMainId();
-                        List<Category> categories = detective.getCategories();
+                        // Массив detectives может иметь не менее одного и не более 3-х объектов
+                        assertTrue(detectives.size() >= 1 && detectives.size() <= 3,
+                                "Ошибка: Массив detectives должен содержать от 1 до 3 объектов");
 
-                        for (Category category : categories) {
-                            int categoryId = category.getCategoryId();
-                            Extra extra = category.getExtra();
+                        for (Detective detective : detectives) {
+                            int mainId = detective.getMainId();
+                            List<Category> categories = detective.getCategories();
 
-                            assertAll("Category",
-                                    () -> {
-                                        // Поле MainId должно быть между 0 и 10
-                                        assertTrue(mainId >= 0 && mainId <= 10,
-                                                "Ошибка: Поле MainId должно быть между 0 и 10");
+                            for (Category category : categories) {
+                                int categoryId = category.getCategoryId();
+                                Extra extra = category.getExtra();
 
-                                        // CategoryID принимает значения 1 или 2
-                                        assertTrue(categoryId == 1 || categoryId == 2,
-                                                "Ошибка: Поле CategoryID должно быть 1 или 2");
+                                assertAll("Category",
+                                        () -> {
+                                            // Поле MainId должно быть между 0 и 10
+                                            assertTrue(mainId >= 0 && mainId <= 10,
+                                                    "Ошибка: Поле MainId должно быть между 0 и 10");
 
-                                        if (categoryId == 2) {
-                                            // Элемент extra может принимать значение null только для CategoryID=2
-                                            assertNull(extra,
-                                                    "Ошибка: Элемент extra должен быть null для CategoryID=2");
-                                        } else {
-                                            // Массив extraArray должен иметь минимум один элемент для CategoryID=1
-                                            assertNotNull(extra,
-                                                    "Ошибка: Элемент extra не должен быть null для CategoryID=1");
-                                            assertFalse(extra.getExtraArray().isEmpty(),
-                                                    "Ошибка: Массив extraArray должен содержать минимум один элемент для CategoryID=1");
+                                            // CategoryID принимает значения 1 или 2
+                                            assertTrue(categoryId == 1 || categoryId == 2,
+                                                    "Ошибка: Поле CategoryID должно быть 1 или 2");
+
+                                            if (categoryId == 2) {
+                                                // Элемент extra может принимать значение null только для CategoryID=2
+                                                assertNull(extra,
+                                                        "Ошибка: Элемент extra должен быть null для CategoryID=2");
+                                            } else {
+                                                // Массив extraArray должен иметь минимум один элемент для CategoryID=1
+                                                assertNotNull(extra,
+                                                        "Ошибка: Элемент extra не должен быть null для CategoryID=1");
+                                                assertFalse(extra.getExtraArray().isEmpty(),
+                                                        "Ошибка: Массив extraArray должен содержать минимум один элемент для CategoryID=1");
+                                            }
                                         }
-                                    }
-                            );
+                                );
+                            }
                         }
-                    }
 
-                    // Поле success принимает значение true только если в массиве detectives есть элемент с firstName ="Sherlock"
-                    boolean hasSherlock = detectives.stream()
-                            .anyMatch(detective -> detective.getFirstName().equals("Sherlock"));
-                    assertEquals(hasSherlock, detectivesResponse.isSuccess(),
-                            "Ошибка: Поле success должно быть true только если в массиве detectives есть элемент с firstName = Sherlock");
-                }
-        );
+                        // Поле success принимает значение true только если в массиве detectives есть элемент с firstName ="Sherlock"
+                        boolean hasSherlock = detectives.stream()
+                                .anyMatch(detective -> detective.getFirstName().equals("Sherlock"));
+                        if (hasSherlock) {assertEquals(hasSherlock, detectivesResponse.isSuccess(),
+                                "Ошибка: Поле success должно быть true только если в массиве detectives есть элемент с firstName = Sherlock");}
+                      }
+            );
+        }
     }
-}
